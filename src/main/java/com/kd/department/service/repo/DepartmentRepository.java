634ccba2b4e136fac1e7ee9aec46614e76c828d1
kutbuddeen <31039586+kutbuddeen@users.noreply.github.com>
2023-12-24 21:15:31 +0530

    initial project configuration

---
+++ src/main/java/com/kd/department/service/repo/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.kd.department.service.repo;
+
+import com.kd.department.service.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
